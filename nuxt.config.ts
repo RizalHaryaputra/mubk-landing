fb33a6c1084f7696920b12f6aa3eff4669ef0162
Rizal Haryaputra <rzlhryptr@gmail.com>
2026-02-04 09:12:12 +0700

    initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Program Bahasa Arab - MUBK',
+      meta: [
+        { name: 'description', content: 'Belajar Bahasa Arab dari Dasar bersama MUBK YPIA' }
+      ]
+    }
+  }
+})
